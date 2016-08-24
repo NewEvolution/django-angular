324abc862769e4baddb61e1209bf3ad9817c034f
Ryan Tanay <ryantanay@newevolution.org>
2016-08-24 10:01:58 -0500

    Removed extraneous module

--- nss/settings.py
+++ nss/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'ng.apps.NgConfig',
     'polls.apps.PollsConfig',
-    'djng',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
