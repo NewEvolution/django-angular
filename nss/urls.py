7433e05eeac8b4e252f32b2d9061d7ca7784d357
Ryan Tanay <ryantanay@newevolution.org>
2016-08-25 09:44:30 -0500

    Modified base urls so that the angular app is at the root of the domain.

--- nss/urls.py
+++ nss/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^ng/', include('ng.urls')),
+    url(r'^', include('ng.urls')),
     url(r'^admin/', admin.site.urls),
 ]
